b267816a2eb97a052655e65e4819e0985508fe0a
Vladislaff Evtushenko <starson4587@gmail.com>
2014-02-26 09:00:43 +0400

    just added not completely task#2 from HW#1
    not work array
    bad format

---
+++ Second Term/1/2/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "stack.h"
+
+using namespace std;
+
+int main()
+{
+	myStacks* left = new pointStack();
+	myStacks* right = new massiveStack();
+	int const length = 100;
+	char* mas = new char[length];
+	cin.getline(mas, length);
+	cout << right->infix(mas) << endl;
+
+	return 0;
+}
+
