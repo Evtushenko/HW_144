44914722a3e81ca66005aa46d5403c8b278c4558
Vladislaff Evtushenko <starson4587@gmail.com>
2014-03-16 11:44:31 +0400

    4.1 added

---
+++ Second Term/CheckMePlz/4,1/HashFunctionFirst.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "hashfunction.h"
+/**
+@brief first hash fucntion = personal class
+*/
+class HashFunctionFirst :
+	public HashFunction
+{
+public:
+	/**
+	@brief example function : returns length*1;
+	@param text from Hash Interface Class
+	*/
+	int calcHash(char* text);
+	HashFunctionFirst(void);
+	~HashFunctionFirst(void);
+};
+
---
+++ Second Term/CheckMePlz/4,1/HashFunctionSecond.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "hashfunction.h"
+/**
+@brief second hash fucntion = personal class
+*/
+class HashFunctionSecond :
+	public HashFunction
+{
+public:
+	/**
+	@brief example function : returns length*2;
+	@param text from Hash Interface Class
+	*/
+	int calcHash(char* text);
+	HashFunctionSecond(void);
+	~HashFunctionSecond(void);
+};
+
