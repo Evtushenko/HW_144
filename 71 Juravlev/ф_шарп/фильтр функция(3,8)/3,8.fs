392937e20b57e1094c84491557d315d7b0f66ae8
Vladislaff Evtushenko <starson4587@gmail.com>
2014-04-10 21:57:26 +0400

    3.8 added

---
+++ 71 Juravlev/ф_шарп/фильтр функция(3,8)/3,8.fs
@@ -0,0 +1,7 @@
+let rec filter con list =
+    match list with
+    | [] -> []
+    | hd::tl -> if (con hd) then hd::(filter con tl) else (filter con tl)
+
+let list = [-2;-1;0;1;2]
+printf "Filter: %A\n" (filter (fun x -> x > 0) list)
