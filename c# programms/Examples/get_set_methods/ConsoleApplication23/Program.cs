55b7185f7e87450d916660d26dcc41ce70fc3229
bdm30 <starson4587@gmail.com>
2014-12-05 22:49:26 +0300

    upd c# example
    // Пример использования get-set
    // Пример вызова консруктора из коструктора при наследовании.
    // Переопделеление и вызов родитесльского метода в дочернем классе

--- c# programms/Examples/get_set_methods/ConsoleApplication23/Program.cs
+++ c# programms/Examples/get_set_methods/ConsoleApplication23/Program.cs
@@ -5,32 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 
 // Пример использования get-set
+// Пример вызова консруктора из коструктора при наследовании.
+// Переопделеление и вызов родитесльского метода в дочернем классе.
 
 namespace ConsoleApplication23
 {
 
     public class Point
     {
-        public Point() {
-            Y = 0;
+        // статическое поле. Можно менять не без испрозований объекта!!!! Можно инициализировать в статическом конструкторе.
+        public static int amount = 2;
+
+        // кокструткор по умолчанию :)
+        //public Point(): this(0,0) { }
+        public Point(int x = 0, int y = 0) // можно умолчание задать и так.
+        {
+            X = x;
+            Y = y;
+        }
+        public int X { get; private set;}
+        public int Y { get; private set;}
+
+        public override string ToString(){
+            return "point: ("+X.ToString() + ";" + Y.ToString()+")";
         }
 
-        public int Y { get; private  set;}
-
-        // поле
-        private int x;
-        private int y;
-        // свойство
-        public int X
+        public static Point Add(Point a, Point b)
         {
-            get { return x; }
-            set { x = value; }
+            Point result = new Point();
+            result.X = a.X + b.X;
+            result.Y = a.Y + b.Y;
+            return result;
         }
-        // метод
-        public void setToOrigin()
+
+        public virtual void say_dimension()
         {
-            X = 0;
-            Y = 46;
+            Console.WriteLine("Base: \"My demension is two!\"");
+        }
+    }
+
+    public class Point_3D : Point {
+        public int Z { get; private set; }
+        public Point_3D() : base(0, 0) { Z = 0; }
+        public Point_3D(int a = 0, int b = 0, int c = 0) : base(a,b) {Z = c;}
+
+        public override void say_dimension()
+        {
+            base.say_dimension();
+            Console.WriteLine("Dirived: \"My is 3 !!!\"");
+        }
+
+        public override string ToString()
+        {
+            return "point: (" + X.ToString() + ";" + Y.ToString() + ";" + Z.ToString() + ")";
         }
     }
 
@@ -38,12 +65,28 @@ namespace ConsoleApplication23
     {
         static void Main(string[] args)
         {
-            Point p = new Point();
-            p.X = 5;
-            p.setToOrigin();
+
+            Console.WriteLine("anonymus");
+            var anon = new {a=1, b = "hello", c =3.5};
+            Console.WriteLine(anon.a);
+            Console.WriteLine(anon.b);
+            Console.WriteLine(anon.c);
+            Console.WriteLine("anonymus");
+
+
+            Point a = new Point(1, 2);
+            Point b = new Point(3, 5);
+            Point sum = Point.Add(a, b);
+
+            Point space = new Point_3D(1,2,3);
             //p.Y = 4;
-            Console.WriteLine(p.X);
-            Console.WriteLine(p.Y);
+            Console.WriteLine(sum);
+            Point.amount = 3;
+            Console.WriteLine(Point.amount);
+            Console.WriteLine(new Point());
+            Console.WriteLine("//////");
+            Console.WriteLine(space);
+            space.say_dimension();
         }
     }
 }
