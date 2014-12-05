4fa880a9e7de75cf0748b26618f935649a8f7484
bdm30 <starson4587@gmail.com>
2014-12-05 21:43:33 +0300

    more examples get-set

--- c# programms/Examples/get_set_methods/ConsoleApplication23/Program.cs
+++ c# programms/Examples/get_set_methods/ConsoleApplication23/Program.cs
@@ -4,19 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Пример использования get-set
+
 namespace ConsoleApplication23
 {
 
     public class Point
     {
-        public Point() { }
+        public Point() {
+            Y = 0;
+        }
 
+        public int Y { get; private  set;}
+
+        // поле
         private int x;
         private int y;
+        // свойство
         public int X
         {
             get { return x; }
             set { x = value; }
+        }
+        // метод
+        public void setToOrigin()
+        {
+            X = 0;
+            Y = 46;
         }
     }
 
@@ -26,7 +40,10 @@ namespace ConsoleApplication23
         {
             Point p = new Point();
             p.X = 5;
+            p.setToOrigin();
+            //p.Y = 4;
             Console.WriteLine(p.X);
+            Console.WriteLine(p.Y);
         }
     }
 }
