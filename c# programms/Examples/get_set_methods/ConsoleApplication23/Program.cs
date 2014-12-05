32845b0120c35234706d8a4fdaace47e4d315d0d
bdm30 <starson4587@gmail.com>
2014-12-05 21:30:13 +0300

    test vpn connection

---
+++ c# programms/Examples/get_set_methods/ConsoleApplication23/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication23
+{
+
+    public class Point
+    {
+        public Point() { }
+
+        private int x;
+        private int y;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point();
+            p.X = 5;
+            Console.WriteLine(p.X);
+        }
+    }
+}
