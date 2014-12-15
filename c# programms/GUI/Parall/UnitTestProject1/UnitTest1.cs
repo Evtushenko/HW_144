43ee7fa5629d7a34084317f0429876422651d6d6
bdm30 <starson4587@gmail.com>
2014-12-15 23:59:06 +0300

    144 gui samples got modul tests

---
+++ c# programms/GUI/Parall/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MyPoint a = new MyPoint(0, 0);
+            MyPoint b = new MyPoint(0, 3);
+            MyPoint c = new MyPoint(4, 3);
+            MyPoint d = new MyPoint(4, 0);
+
+            MyPoint e = new MyPoint(2, 2);
+
+            myParallelogram parall = new myParallelogram(a, b, c, d);
+
+            Assert.AreEqual(true, parall.isCorrect(), "correct data chelking");
+            parall = new myParallelogram(a, b, e, d);
+            Assert.AreEqual(false, parall.isCorrect(), "incorrect data chelking");
+        }
+    }
+}
---
+++ c# programms/GUI/trinagle/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MyPoint a = new MyPoint(0, 0);
+            MyPoint b = new MyPoint(4, 6);
+            MyPoint c = new MyPoint(4, 0);
+
+            MyPoint in_point = new MyPoint(3, 2);
+            MyPoint ex_point = new MyPoint(8, 8);
+
+            MyTriangle triangle = new MyTriangle(a,b,c);
+
+            Assert.AreEqual(true, triangle.MyPointBelong(in_point), "internal point checking");
+            Assert.AreEqual(false, triangle.MyPointBelong(ex_point), "external point checking");
+        }
+    }
+}
